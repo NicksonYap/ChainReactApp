898d97b610a663c04949c93b3f2844af8573be7c
Nickson Yap <hi@nickson.me>
2018-09-04 06:17:36 +0800

    React Native Upgrade

--- ios/ChainReactApp/AppDelegate.m
+++ ios/ChainReactApp/AppDelegate.m
@@ -1,10 +1,8 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #import "AppDelegate.h"
@@ -25,36 +23,7 @@
 {
   NSURL *jsCodeLocation;
 
-  [RNCrashes registerWithCrashDelegate: [[RNCrashesDelegateAlwaysSend alloc] init]];  // Initialize Mobile Center crashes
-
-  [RNAnalytics registerWithInitiallyEnabled:true];  // Initialize Mobile Center analytics
-
-  
-#ifdef DEBUG
-    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-#else
-    jsCodeLocation = [CodePush bundleURL];
-#endif
-
-	RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-																											moduleName:@"ChainReactApp"
-																							 initialProperties:nil
-																									 launchOptions:launchOptions];
-
-	//[RCTSplashScreen open:rootView];
-	[RCTSplashScreen open:rootView withImageNamed:@"splash"]; // activate splashscreen, imagename from LaunchScreen.xib
-
-	rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-	self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-	UIViewController *rootViewController = [UIViewController new];
-	rootViewController.view = rootView;
-	self.window.rootViewController = rootViewController;
-	[self.window makeKeyAndVisible];
-  [self probeFonts];
-
-	return YES;
-}
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
 // Required to register for notifications
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
